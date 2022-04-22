f612498e053af455b9eb490b5b06a81f15401e74
myungsangBaek <bms6168@naver.com>
2022-04-22 15:38:06 +0900

    [FEATURE] Generate apollo client

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
