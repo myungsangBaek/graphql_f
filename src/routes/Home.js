f6d075c41ef26ced463a71436053942a002d6449
myungsangBaek <bms6168@naver.com>
2022-04-22 16:50:59 +0900

    [FEATURE] Set proxy, get data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 const Home = () => {
-  return <h1>{"Home"}</h1>;
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
 };
+
 export default Home;
